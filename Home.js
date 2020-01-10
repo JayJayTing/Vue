d02019ef68a41b24f049aa3b567938a87c38aa59
Jay Jay <jayjay@Jays-MacBook-Pro.local>
2020-01-10 17:00:05 -0500

    links

--- Home.js
+++ Home.js
@@ -2,14 +2,18 @@ new Vue({
   el: '#app',
   data: {
     title: 'test',
-    link: 'http://www.google.com'
+    link: 'http://www.google.com',
+    finishedLink: '<a href="wwww.google.com">GOOGLE</a>'
   },
   methods: {
     changeTitle: function(e) {
+      console.log(e.target.value);
       this.title = e.target.value;
     },
     sayHello: function() {
-      return this.title;
+      //this.title = 'hello';
+      console.log('sayHElo');
+      return 'testing';
     },
     hello: function() {
       console.log('testing');
