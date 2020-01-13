725719ab30df04b3dc1a1184ff9e2eeaa366e63e
JayJayTing <tingjayj@gmail.com>
2020-01-13 05:11:38 -0500

    new things to add

--- Home.js
+++ Home.js
@@ -1,24 +1,34 @@
 new Vue({
-  el: '#app',
-  data: {
-    title: 'test',
-    link: 'http://www.google.com',
-    finishedLink: '<a href="wwww.google.com">GOOGLE</a>'
-  },
-  methods: {
-    changeTitle: function(e) {
-      console.log(e.target.value);
-      this.title = e.target.value;
-    },
-    sayHello: function() {
-      //this.title = 'hello';
-      console.log('sayHElo');
-      return 'testing';
-    },
-    hello: function() {
-      console.log('testing');
-    }
-  }
+	el: "#app",
+	data: {
+		title: "test",
+		count: 0,
+		link: "http://www.google.com",
+		finishedLink: '<a href="wwww.google.com">GOOGLE</a>',
+		x: 0,
+		y: 0
+	},
+	methods: {
+		changeTitle: function(e) {
+			console.log(e.target.value);
+			this.title = e.target.value;
+		},
+		sayHello: function() {
+			//this.title = 'hello';
+			console.log("sayHElo");
+			return "testing";
+		},
+		hello: function() {
+			console.log("testing");
+		},
+		increase() {
+			return this.count++;
+		},
+		updateCoordinates: function(e) {
+			this.x = e.clientX;
+			this.y = e.clientY;
+		}
+	}
 });
 
 //within new Vue() the this keyword is proxied to data property within the object
